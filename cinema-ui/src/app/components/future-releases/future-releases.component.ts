1925a68c50a5d1a2d5ed21b0db8644c2ae1dddee
Daniel Smith <daniel.smith@academytrainee.com>
2018-06-26 17:22:55 +0100

    All pages made, routing links set up

---
+++ cinema-ui/src/app/components/future-releases/future-releases.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-future-releases',
+  templateUrl: './future-releases.component.html',
+  styleUrls: ['./future-releases.component.css']
+})
+export class FutureReleasesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ cinema-ui/src/app/components/opening-times/opening-times.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-opening-times',
+  templateUrl: './opening-times.component.html',
+  styleUrls: ['./opening-times.component.css']
+})
+export class OpeningTimesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
