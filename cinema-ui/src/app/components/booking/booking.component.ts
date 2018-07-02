f14fe0e7fff160a117057e87fb9bac80d740d900
SamHunnable <samhunnable@academytrainee.com>
2018-07-02 17:00:10 +0100

    Added some front end checks

--- cinema-ui/src/app/components/booking/booking.component.ts
+++ cinema-ui/src/app/components/booking/booking.component.ts
@@ -12,6 +12,7 @@ export class BookingComponent implements OnInit {
   bookingForm: FormGroup;
   showing: {[k: string]: any} = {};
   booking: {[k: string]: any} = {};
+  bookingState: boolean = true;
 
   constructor(private bookingService: BookingService, private route: ActivatedRoute) { }
 
@@ -23,15 +24,33 @@ export class BookingComponent implements OnInit {
       err => console.log('err'),
       () => console.log('Showing loaded')
     );
+    this.booking.adultTickets = 0;
+    this.booking.concessionTickets = 0;
+    this.booking.childTickets = 0;
   }
 
   submitBooking(){
-    console.log("showing id = "this.showing.id)
-    this.booking.showingId = this.showing.id;
-    this.bookingService.createBooking(this.booking).subscribe(
-      response => console.log(response),
-      err => console.log(err)
-    );
+    if (this.booking.adultTickets + this.booking.concessionTickets + this.booking.childTickets <= 0) {
+      window.alert("Booking must contain a ticket");
+    }
+
+    else if (this.booking.adultTickets + this.booking.concessionTickets + this.booking.childTickets > 10) {
+      window.alert("Bookings cannot have more than 10 tickets");
+    }
+
+    else if (this.booking.customerName == null) {
+      window.alert("Booking must have a name");
+    }
+    else {
+      console.log("showing id = "this.showing.id)
+      this.booking.showingId = this.showing.id;
+      this.bookingService.createBooking(this.booking).subscribe(
+        response => console.log(response),
+        err => console.log(err)
+      );
+      this.bookingState = false;
+    }
+
   }
 
 }
