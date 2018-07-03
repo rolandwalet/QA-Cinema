c2631278cbae5bfec4575d397bbc093459606ecd
SamHunnable <samhunnable@academytrainee.com>
2018-07-03 10:33:31 +0100

    Functionality for front end added. Needs some styling/redesign

--- cinema-ui/src/app/components/future-releases/future-releases.component.ts
+++ cinema-ui/src/app/components/future-releases/future-releases.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FilmService } from '../../services/film/film.service';
+
 
 @Component({
   selector: 'app-future-releases',
@@ -6,10 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./future-releases.component.css']
 })
 export class FutureReleasesComponent implements OnInit {
+  films: Array<any>;
 
-  constructor() { }
+  constructor( private filmService: FilmService ) { }
 
   ngOnInit() {
-  }
+    this.filmService.getFutureFilms().subscribe(data => {
+      this.films = data;
+      console.log(data);
+    });
 
+  }
+  populateFilms() {
+    var showingIndex = 0;
+    var currentFilmID;
+    var currentDate;
+    var el = document.getElementById('pageContent');
+    el.innerHTML = "";
+    var htmlToAdd = "";
+    for (let index = 0; index < this.films.length; ++index) {
+      console.log(index);
+
+      el.innerHTML +=  "<img style=\"width: 70%; max-width: 600px; height: auto;\" src=\"" + this.films[index].poster + "\"/>" ;
+      el.innerHTML += "<br />";
+      el.innerHTML += "<h3>" + this.films[index].title + "</h3>";
+      el.innerHTML += "<p><b>Run time: </b>" + this.films[index].runTime + " minutes </p>";
+      el.innerHTML += "<p>" + this.films[index].synopsis + "</p>";
+      el.innerHTML += "<p><b>Starring: </b>" + this.films[index].actors + "</p>";
+      el.innerHTML += "<p><b>Directed By: </b>" + this.films[index].director.name + "</p>";
+
+      }
+      currentFilmID = this.films[index].id;
+      currentDate = "";
+      htmlToAdd += "</table>";
+      el.innerHTML += htmlToAdd;
+      htmlToAdd = "";
+      showingIndex = 0;
+    }
 }
